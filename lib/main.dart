674a0a656b0f978e1a41fb5bae83baab3edbd233
freelife1191 <freelife1191.good@gmail.com>
2019-05-26 00:21:35 +0900

    프로젝트 초기 셋팅

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:fluttercookbook/src/MyAnimatedOpacity.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Tutorial'),
+        ),
+        body: Home()
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        /*
+        ListTile(
+          title: Text('AnimatedContainer'),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => MyAnimatedContainer()));
+          },
+        ),
+        */
+        ListTile(
+          title: Text('AnimatedOpacity'),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => MyAnimatedOpacity()));
+          },
+        ),
+      ],
+    );
+  }
+}
+
