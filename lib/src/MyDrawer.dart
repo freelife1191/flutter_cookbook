e0a2d5f0e033df9f3d1ba568594812a2286b7eb4
freelife1191 <freelife1191.good@gmail.com>
2019-05-26 19:50:12 +0900

    MyDrawer 구현

--- lib/src/MyDrawer.dart
+++ lib/src/MyDrawer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/**
+ * 네비게이션 Drawer
+ */
 class MyDrawer extends StatefulWidget {
   @override
   _MyDrawerState createState() => _MyDrawerState();
@@ -9,6 +12,40 @@ class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero, //공백을 메꾸기 위해
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Header'),
+              decoration: BoxDecoration( // DrawerHeader은 color 속성이 없으므로 decoration 으로 색깔을 넣어줌
+                color: Colors.blue
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // 할 일
+                Navigator.pop(context); //네비게이션 닫기
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // 할 일
+                Navigator.pop(context); //네비게이션 닫기
+              },
+            ),
+            ListTile(
+              title: Text('Item 3'),
+              onTap: () {
+                // 할 일
+                Navigator.pop(context); //네비게이션 닫기
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Drawer'),
       ),
