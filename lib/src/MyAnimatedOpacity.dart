2e13ba44ca0dad8ab41afa8508f981b3bc8b2196
freelife1191 <freelife1191.good@gmail.com>
2019-05-26 17:50:21 +0900

    AnimatedOpacity

--- lib/src/MyAnimatedOpacity.dart
+++ lib/src/MyAnimatedOpacity.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/**
+ * 투명도만 조절하는 위젯
+ */
 class MyAnimatedOpacity extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _MyAnimatedOpacityState();
@@ -7,14 +10,31 @@ class MyAnimatedOpacity extends StatefulWidget {
 }
 
 class _MyAnimatedOpacityState extends State<MyAnimatedOpacity> {
+  bool _visible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('AnimatedOpacity'),
       ),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.play_arrow), onPressed: null),
+      // opacity 와 duration 이 필수
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(milliseconds: 500), // seconds 옵션
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.play_arrow), onPressed: () {
+        setState(() { // 반대값으로 변환하는 함수
+          _visible = !_visible;
+        });
+      }),
     );
   }
 }
